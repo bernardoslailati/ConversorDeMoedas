0b31f5a0061e71032bfd3eb0830fc59864b78def
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-10-13 19:59:34 +0200

    Mostrando a lista de tipos de moedas na tela

--- app/src/main/java/com/dev/bernardoslailati/conversordemoedas/MainActivity.kt
+++ app/src/main/java/com/dev/bernardoslailati/conversordemoedas/MainActivity.kt
@@ -9,17 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import com.dev.bernardoslailati.conversordemoedas.network.KtorHttpClient
+import com.dev.bernardoslailati.conversordemoedas.databinding.ActivityMainBinding
+import com.dev.bernardoslailati.conversordemoedas.ui.CurrencyTypesAdapter
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel by viewModels<CurrencyExchangeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(
@@ -33,11 +37,9 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.apply {
             launch {
                 viewModel.currencyTypes.collect { result ->
-                    result.onSuccess {
-                        Toast.makeText(
-                            this@MainActivity, it.size.toString(),
-                            Toast.LENGTH_LONG
-                        ).show()
+                    result.onSuccess { currencyTypes ->
+                        binding.spnFromExchange.adapter = CurrencyTypesAdapter(currencyTypes)
+                        binding.spnToExchange.adapter = CurrencyTypesAdapter(currencyTypes)
                     }.onFailure {
                         Toast.makeText(
                             this@MainActivity, it.message,
